43842d3140eaba36542f8b27eb57c437e17266a2
Brittani <96275616+Serendipbrity@users.noreply.github.com>
2022-03-31 17:36:29 -0400

    error messeges

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -24,15 +24,27 @@ var getUserRepos = function(user) {
    var apiUrl = "http://api.github.com/users/" + user + "/repos";
 
    fetch(apiUrl).then(function(response) {
-       console.log(response);
+       // request was successful
+       if (response.ok){
        response.json().then(function(data) {
         displayRepos(data, user);
        });
+    } else {
+        alert("Error: Github User Not Found");
+    }
+    })
+    .catch(function(error) {
+       alert("Unable to connect to Github");
     });
 };
 
 // search term is the user input
 var displayRepos = function(repos, searchTerm) {
+    // check if api returned any repos
+    if (repos.length === 0) {
+        repoContainerEl.textContent = "No repositories found.";
+        return;
+    }
     console.log(repos);
     console.log(searchTerm);
     // clear old content
