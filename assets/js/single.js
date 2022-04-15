a3e5991b032928e753dd6b22ba6b9ae8bb7fdd96
Brittani <96275616+Serendipbrity@users.noreply.github.com>
2022-04-15 14:02:52 -0400

    repos

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,22 +1,39 @@
+var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
+var getRepoName = function() {
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+
+    if (repoName) {
+        // display repo name on the page
+        repoNameEl.textContent = repoName;
+    getRepoIssues(repoName);
+} else {
+    document.location.replace("./index.html");
+}
+};
 
 var getRepoIssues = function(repo) {
     console.log(repo);
     // create variable to hold query
-    var apiUrl = "http://api.github.com/repos/" + repo + "/issues?direction=asc";
+    var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
     fetch(apiUrl).then(function(response) {
        // request was successful
        if (response.ok) {
            response.json().then(function(data){
                displayIssues(data);
-               
+               // check if api has paginated issues
+               if (response.headers.get("Link")) {
+                   displayWarning(repo);
+                   console.log("repo has more than 30 issues");
+               }
            });
        }
        else {
-           console.log(response);
-           alert("There was a problem with your request!");
+         document.location.replace("./index.html");
     }
  });
 };
@@ -28,7 +45,7 @@ var displayIssues = function(issues) {
     }
 
     // loop over given issues
-    for (var i = 0; i < issues.length; i ++) {
+    for (var i = 0; i < issues.length; i++) {
         // create a link to take users to the issue on github
         var issueEl = document.createElement("a");
         issueEl.classList = "list-item flex-row justify-space-between align-center";
@@ -59,4 +76,17 @@ var displayIssues = function(issues) {
     }
 };
 
-getRepoIssues("facebook/react");
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "Github.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+  
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+  };
+
+getRepoName();
